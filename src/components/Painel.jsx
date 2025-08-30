67504580a25e897b83874c809c48cb137a5cf736
tpinaa <tatiana.dcp@gmail.com>
2025-08-30 10:41:17 -0300

    renderizacao atualizacao dados

--- src/components/Painel.jsx
+++ src/components/Painel.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import CardHistorico from "./CardHistorico"
 function Painel() {
+    const [listaIndicadores, setlistaIndicadores] = useState([])
+    useEffect(() => {
+        async function getIndicadores() {
+            const indicadores = await fetch("/api/indicador/")
+            return indicadores
+        }
+        const indicadores = getIndicadores()
+        setlistaIndicadores(indicadores)
+    }, []);
+    console.log(listaIndicadores)
+
     return (
         <>
-            <CardHistorico title={"IGPUB"}/>
+            <CardHistorico title={"IGPUB"} />
         </>
     )
 }
